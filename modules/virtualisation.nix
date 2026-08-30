d3968af9ca30a5a4daad3cebb55bf2cbe4a155e7
Hqb123456 <94301495@qq.com>
2026-08-30 16:49:55 +0800

    添加虚拟机管理程序

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -18,6 +18,15 @@
   # Incus（LXD 社区继承者）
   virtualisation.incus.enable = true;
 
+  # 启用 virt-manager 程序
+  programs.virt-manager.enable = true;
+
+  # 启用 libvirt 虚拟化守护进程
+  virtualisation.libvirtd.enable = true;
+
+  # （可选）启用 SPICE USB 重定向，提升虚拟机USB设备体验
+  virtualisation.spiceUSBRedirection.enable = true;
+
   # 用户组
-  users.users.shorin.extraGroups = [ "docker" "incus-admin" ];
+  users.users.shorin.extraGroups = [ "libvirtd" "docker" "incus-admin" ];
 }
